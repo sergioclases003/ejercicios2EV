073453cfb95e0f8dd4af26467c08254dd1ff66aa
sergioclases003 <sergioclases003@gmail.com>
2026-01-19 11:05:51 +0100

    ejercicio 9

---
+++ src2/CancionStats9.java
@@ -0,0 +1,31 @@
+package ejercicios;
+
+public class CancionStats9 {
+    private String titulo;
+    private int numeroReproducciones;
+
+    public CancionStats9(String titulo) {
+        this.titulo = titulo;
+        this.numeroReproducciones = 0;
+    }
+
+    public void reproducir() {
+        this.numeroReproducciones++;
+    }
+
+    public int getNumeroReproducciones() {
+        return numeroReproducciones;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public static void main(String[] args) {
+        CancionStats9 cancion = new CancionStats9("Bohemian Rhapsody");
+        for (int i = 0; i < 5; i++) {
+            cancion.reproducir();
+        }
+        System.out.println("La canción '" + cancion.getTitulo() + "' tiene un total de " + cancion.getNumeroReproducciones() + " reproducciones.");
+    }
+}
