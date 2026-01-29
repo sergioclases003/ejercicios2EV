0c6a977bbebc81005a8422d13e1b02fab2416fc9
sergioclases003 <sergioclases003@gmail.com>
2026-01-29 13:04:37 +0100

    Update Main.java

--- src2/Main.java
+++ src2/Main.java
@@ -7,47 +7,62 @@ public class Main {
         System.out.println("Ejecutando Ejercicio 1:");
         Cancion1.main(args);
 
+        
         System.out.println("\nEjecutando Ejercicio 2:");
         Pelicula2.main(args);
+
 
         System.out.println("\nEjecutando Ejercicio 3:");
         Youtuber3.main(args);
 
+
         System.out.println("\nEjecutando Ejercicio 4:");
         JugadorFutbol4.main(args);
+
 
         System.out.println("\nEjecutando Ejercicio 5:");
         Skin5.main(args);
 
+
         System.out.println("\nEjecutando Ejercicio 6:");
         Pokemon6.main(args);
+
 
         System.out.println("\nEjecutando Ejercicio 7:");
         Manga7.main(args);
 
+
         System.out.println("\nEjecutando Ejercicio 8:");
         EquipoEsports8.main(args);
+
 
         System.out.println("\nEjecutando Ejercicio 9:");
         CancionStats9.main(args);
 
+
         System.out.println("\nEjecutando Ejercicio 10:");
         Marcador10.main(args);
+
 
         System.out.println("\nEjecutando Ejercicio 11:");
         BateriaMovil11.main(args);
 
+
         System.out.println("\nEjecutando Ejercicio 12:");
         personaje12.main(args);
+
 
         System.out.println("\nEjecutando Ejercicio 13:");
         carrito13.main(args);
 
+
         System.out.println("\nEjecutando Ejercicio 14:");
         post14.main(args);
 
+
         System.out.println("\nEjecutando Ejercicio 15:");
         SesionJuego15.main(args);
+
 
         System.out.println("\nEjecutando Ejercicio 16:");
         EntradasCine16.main(args);
@@ -70,7 +85,7 @@ public class Main {
                 "        SEMANA 2 - EJERCICIOS");
         System.out.println("\n");
 
-        
+
         /*
         ================================================================================
         */
